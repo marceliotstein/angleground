14af8ce0166f48c89f215f86d3d4dd1e65b896a6
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2015-02-15 12:03:40 -0500

    testing popovers and carousels

---
+++ ab-popover.js
@@ -0,0 +1,21 @@
+angular.module('projectStore').controller('PopoverDemoCtrl', function ($scope) {
+  $scope.dynamicPopover = 'Hello, World!';
+  $scope.dynamicPopoverTitle = 'Title';
+  $scope.dynamicPopoverImage = "http://placekitten.com/g/300/300";
+});
+
+
+
+angular.module('projectStore')
+    .directive("popoverHtmlUnsafePopup", function () {
+      return {
+        restrict: "EA",
+        replace: true,
+        scope: { title: "@", content: "@", placement: "@", animation: "&", isOpen: "&" },
+        templateUrl: "popover-html.html"
+      };
+    })
+
+    .directive("popoverHtmlUnsafe", [ "$tooltip", function ($tooltip) {
+      return $tooltip("popoverHtmlUnsafe", "popover", "click");
+    }]);
